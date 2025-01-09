e65a8abb422ae4b4bdb1f709985f7420a94f88b5
kHALIFA-AFK <Khalifah.Alnisof@students.iaac.net>
2025-01-09 19:42:10 +0300

    after-break development

--- scriptcode.js
+++ scriptcode.js
@@ -1,25 +1,56 @@
-console.log('hello')
+// console.log('hello')
 
-const myConstVar = 5
+// const myConstVar = 5
 
 
-let foo = 93
-console.log(foo)
+// let foo = 93
+// console.log(foo)
 
-foo = "my string"
-console.log(foo)
+// foo = "my string"
+// console.log(foo)
 
-let foodArray = ["pizza", "pasta", "salad"]
-console.log(foodArray)
+// let foodArray = ["pizza", "pasta", "salad"]
+// console.log(foodArray)
 
-let foodItem = foodArray[1]
-console.log("foodItem:" , foodItem)
+// let foodItem = foodArray[1]
+// console.log("foodItem:" , foodItem)
 
-let objectVariable = {
-    key: 'value',
-    foods: foodArray
+// let objectVariable = {
+//     key: 'value',
+//     foods: foodArray
+// }
+// console.log(objectVariable)
+
+// let foods = objectVariable['foods']
+// console.log('foods:', foods)
+
+// function AddTwoNumbers(num1, num2){
+//     let sum = num1 + num2
+//     return sum
+// }
+
+// let mySum = AddTwoNumbers(1, 2)
+// console.log('mySum:', mySum)
+
+let breakfastElement = document.getElementById("breakfast")
+console.log(breakfastElement)
+
+function breakfastClicked(){
+    console.log('breakfast element was clicked')
+    let breakfastFoodsDiv = document.getElementById('breakfastFoods')
+    breakfastFoodsDiv.style.display = 'flex'
+    let lunchFoodsDiv = document.getElementById('lunchFoods')
+    lunchFoodsDiv.style.display = 'none'
 }
-console.log(objectVariable)
 
-let foods = objectVariable['foods']
-console.log('foods:', foods)
+function lunchClicked(){
+    let breakfastFoodsDiv = document.getElementById('breakfastFoods')
+    breakfastFoodsDiv.style.display = 'none'
+    let lunchFoodsDiv = document.getElementById('lunchFoods')
+    lunchFoodsDiv.style.display = 'flex'
+}
+
+breakfastElement.addEventListener("click", () => breakfastClicked())
+
+let lunchElement = document.getElementById('lunch')
+lunchElement.addEventListener("click", () => lunchClicked())
